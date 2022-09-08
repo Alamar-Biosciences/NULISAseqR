3b9869464dbbc3085ee826692cd9e9c6eb996e88
Joanne Beer <joannecbeer@gmail.com>
2022-09-08 16:10:59 -0700

    modify sample code so roxygenise() can run

--- R/plateHeatmap.R
+++ R/plateHeatmap.R
@@ -14,10 +14,12 @@
 #' @export
 #' 
 # NEEDS EDITING
-m <- t(matrix(as.numeric(countMatrix1[NIC,3:98]), nrow=8, ncol=12, byrow=TRUE)[8:1,])
-image(m,
-      xaxt='n', yaxt='n', main=paste0('NextSeq Run #1 ', countMatrix1$BarcodeName[NIC]), 
-      col=hcl.colors(12, "YlOrRd", rev = TRUE))
-for (x in 1:nrow(m))
-  for (y in 1:ncol(m))
-    text(1/(nrow(m)-1)*(x-1), 1/(ncol(m)-1)*(y-1), m[x,y], cex=0.7)
+plateHeatmap <- function(data_matrix){
+  m <- t(matrix(as.numeric(countMatrix1[NIC,3:98]), nrow=8, ncol=12, byrow=TRUE)[8:1,])
+  image(m,
+        xaxt='n', yaxt='n', main=paste0('NextSeq Run #1 ', countMatrix1$BarcodeName[NIC]), 
+        col=hcl.colors(12, "YlOrRd", rev = TRUE))
+  for (x in 1:nrow(m))
+    for (y in 1:ncol(m))
+      text(1/(nrow(m)-1)*(x-1), 1/(ncol(m)-1)*(y-1), m[x,y], cex=0.7)
+}
