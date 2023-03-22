e6ac3b3b3f09ede6d055646e6dfe5c61a9c2164b
dwightkuo <22648143+dwightkuo@users.noreply.github.com>
2023-03-21 17:57:46 -0700

    add pdf to output formats for skeleton.Rmd within API...still needs some formatting work though

--- inst/plumber/API_1/plumber.R
+++ inst/plumber/API_1/plumber.R
@@ -15,16 +15,15 @@ normXML <- function(in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"
 #* @param IPC Name to search for Interprocess control (IPC) samples
 #* @param NC Name to search for Negative control (NC) samples
 #* @param IC Name to search for Internal Control (IC) targets
-#* @param output_format Output format to return (e.g. html_document, pdf_document)
 #* @serializer html
-#* @post /xml2report
-xml2report <- function(res, in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"), IC=c("mCherry"), output_format="html_document"){
+#* @post /xml2html
+xml2html <- function(res, in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"), IC=c("mCherry")){
   future_promise({
     UUID <- UUIDgenerate()
     tempFile <- paste0(UUID, '.Rmd')
-    outFile <- paste0(UUID, if(output_format == "html_document") ".html" else ".pdf")
+    outFile <- paste0(UUID, ".html")
     file.copy('skeleton.Rmd', tempFile)
-    rmarkdown::render('skeleton.Rmd', output_format=output_format, 
+    rmarkdown::render(tempFile, output_format="html_document", 
                                       output_file=outFile,
                                       params=list(
                                                   xmlFiles=toString(in_xml),
@@ -36,6 +35,36 @@ xml2report <- function(res, in_xml, IPC=c("InterProcessControl"), NC=c("Negative
                                                   ))
     unlink(tempFile)
     readBin(outFile, "raw", n=file.info(outFile)$size)
+  })
+}
+
+#* @param in_xml:file Character string. Path and name of the file.
+#* @param IPC Name to search for Interprocess control (IPC) samples
+#* @param NC Name to search for Negative control (NC) samples
+#* @param IC Name to search for Internal Control (IC) targets
+#* @serializer contentType list(type="application/pdf")
+#* @post /xml2pdf
+xml2pdf <- function(res, in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"), IC=c("mCherry")){
+  future_promise({
+    UUID <- UUIDgenerate()
+    tempFile <- paste0(UUID, '.Rmd')
+    outFile <- paste0(UUID, ".html")
+    outFilePDF <- paste0(UUID, ".pdf")
+    file.copy('skeleton.Rmd', tempFile)
+    rmarkdown::render(tempFile, output_format="html_document", 
+                                      output_file=outFile,
+                                      params=list(
+                                                  xmlFiles=toString(in_xml),
+                                                  dataDir=NULL,
+                                                  type="WebApp",
+                                                  IPC=IPC, 
+                                                  NC=NC, 
+                                                  IC=IC
+                                                  ))
+    unlink(tempFile)
+    chrome_print(outFile, outFilePDF)
+    unlink(outFile)
+    readBin(outFilePDF, 'raw', n=file.info(outFilePDF)$size)
   })
 }
 
