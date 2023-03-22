45c8155e5ca2c56f97e0d7b30b293be38535d0a9
dwightkuo <22648143+dwightkuo@users.noreply.github.com>
2023-03-21 17:17:36 -0700

    first working version of plumber API which can generate skeleton.Rmd reports

--- inst/plumber/API_1/plumber.R
+++ inst/plumber/API_1/plumber.R
@@ -12,15 +12,30 @@ normXML <- function(in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"
 }
 
 #* @param in_xml:file Character string. Path and name of the file.
-#* @serializer text
-#* @post /xml2doc
-xml2doc <- function(in_xml){
+#* @param IPC Name to search for Interprocess control (IPC) samples
+#* @param NC Name to search for Negative control (NC) samples
+#* @param IC Name to search for Internal Control (IC) targets
+#* @param output_format Output format to return (e.g. html_document, pdf_document)
+#* @serializer html
+#* @post /xml2report
+xml2report <- function(res, in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"), IC=c("mCherry"), output_format="html_document"){
   future_promise({
-    return(rmarkdown::render('skeleton.Rmd', params=list(
-                                                         xmlFiles=in_xml,
-                                                         dataDir=".",
-                                                         type="webapp"
-                                                         )))
+    UUID <- UUIDgenerate()
+    tempFile <- paste0(UUID, '.Rmd')
+    outFile <- paste0(UUID, if(output_format == "html_document") ".html" else ".pdf")
+    file.copy('skeleton.Rmd', tempFile)
+    rmarkdown::render('skeleton.Rmd', output_format=output_format, 
+                                      output_file=outFile,
+                                      params=list(
+                                                  xmlFiles=toString(in_xml),
+                                                  dataDir=NULL,
+                                                  type="WebApp",
+                                                  IPC=IPC, 
+                                                  NC=NC, 
+                                                  IC=IC
+                                                  ))
+    unlink(tempFile)
+    readBin(outFile, "raw", n=file.info(outFile)$size)
   })
 }
 
