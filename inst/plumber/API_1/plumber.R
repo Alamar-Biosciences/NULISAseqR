a29463968deaebae53cbd3383ca19d6ece9cd815
Kasun Buddika <jkkbuddika@gmail.com>
2023-04-25 15:24:49 -0700

    Update API: Improve xml2pdf endpoint, needs more work

--- inst/plumber/API_1/plumber.R
+++ inst/plumber/API_1/plumber.R
@@ -2,6 +2,11 @@
 # NULISAseqR API #
 ##################
 
+# Define global vars and other options
+rmd_path <- "/workingDir/NULISAseqR/inst/rmarkdown/templates/nulisaseq/skeleton/skeleton.Rmd"
+options(pagedown.remote.maxattempts = 40)
+options(pagedown.remote.sleeptime = 2)
+
 #* @apiTitle NULISAseqR Reporting
 #* @apiDescription RESTful API for NULISAseqR data analysis, reporting and related operations
 #* @apiVersion 1.0.0
@@ -14,9 +19,6 @@ function(req) {
         req$HTTP_USER_AGENT, "@", req$REMOTE_ADDR, "\n")
     plumber::forward()
 }
-
-# Define Rmarkdown file path for endpoints
-rmd_path <- "/workingDir/NULISAseqR/inst/rmarkdown/templates/nulisaseq/skeleton/skeleton.Rmd"
 
 #* @param in_xml:file Character string. Path and name of the file.
 #* @param IPC Name to search for Interprocess control (IPC) samples
@@ -72,33 +74,48 @@ xml2html <- function(res,
   })
 }
 
-#* @param in_xml:file Character string. Path and name of the file.
+#* @param in_xml:[file] Character string. Path and name of the file.
 #* @param IPC Name to search for Interprocess control (IPC) samples
 #* @param NC Name to search for Negative control (NC) samples
 #* @param IC Name to search for Internal Control (IC) targets
+#* @param study_name Name of the study
+#* @param assayName Name of the assay
 #* @serializer contentType list(type="application/pdf")
 #* @post /xml2pdf
-xml2pdf <- function(res, in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"), IC=c("mCherry")){
-  future_promise({
-    UUID <- UUIDgenerate()
-    tempFile <- paste0(UUID, '.Rmd')
+xml2pdf <- function(res,
+                    in_xml,
+                    IPC = c("InterProcessControl"),
+                    NC = c("NegativeControl"),
+                    IC = c("mCherry"),
+                    study_name = "Study Name",
+                    assayName = "NULISAseq 200-plex Inflammation Panel") {
+  promises::future_promise({
+    UUID <- uuid::UUIDgenerate()
+    tempFile <- paste0(UUID, ".Rmd")
     outFile <- paste0(UUID, ".html")
     outFilePDF <- paste0(UUID, ".pdf")
     file.copy(rmd_path, tempFile)
-    rmarkdown::render(tempFile, output_format="html_document", 
-                                      output_file=outFile,
-                                      params=list(
-                                                  xmlFiles=toString(in_xml),
-                                                  dataDir=NULL,
-                                                  reportType="WebApp",
-                                                  IPC=IPC, 
-                                                  NC=NC, 
-                                                  IC=IC
-                                                  ))
+
+    # Handle multiple XML inputs
+    xml_files_list <- lapply(in_xml, toString)
+    xml_files_vec <- as.character(unlist(xml_files_list))
+
+    rmarkdown::render(tempFile,
+                      output_format = "html_document",
+                      output_file = outFile,
+                      params = list(xmlFiles = xml_files_vec,
+                                    dataDir = NULL,
+                                    reportType = "WebApp",
+                                    IPC = IPC,
+                                    NC = NC,
+                                    IC = IC,
+                                    study_name = study_name,
+                                    assayName = assayName))
     unlink(tempFile)
-    chrome_print(outFile, outFilePDF)
+    chrome_path <- Sys.which("chromium-browser")
+    pagedown::chrome_print(outFile, outFilePDF, extra_args = c("--no-sandbox", "--disable-gpu"), browser = chrome_path, verbose = 2) # nolint
     unlink(outFile)
-    readBin(outFilePDF, 'raw', n=file.info(outFilePDF)$size)
+    readBin(outFilePDF, "raw", n = file.info(outFilePDF)$size)
   })
 }
 
