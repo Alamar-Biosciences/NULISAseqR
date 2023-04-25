60eaf8a61af4a1eb3e5251498fd3874d29c56d79
Kasun Buddika <jkkbuddika@gmail.com>
2023-04-25 15:41:54 -0700

    Formatting changes to the API and assciated methods/endpoints

--- inst/plumber/API_1/plumber.R
+++ inst/plumber/API_1/plumber.R
@@ -1,3 +1,8 @@
+# =========================================================================
+# Copyright © 2023 Alamar Biosciences, Inc.
+# NULISAseqR Data Analysis and Reporting API
+# =========================================================================
+
 ##################
 # NULISAseqR API #
 ##################
@@ -7,7 +12,7 @@ rmd_path <- "/workingDir/NULISAseqR/inst/rmarkdown/templates/nulisaseq/skeleton/
 options(pagedown.remote.maxattempts = 40)
 options(pagedown.remote.sleeptime = 2)
 
-#* @apiTitle NULISAseqR Reporting
+#* @apiTitle NULISAseqR Analysis/Reporting
 #* @apiDescription RESTful API for NULISAseqR data analysis, reporting and related operations
 #* @apiVersion 1.0.0
 
@@ -20,6 +25,17 @@ function(req) {
     plumber::forward()
 }
 
+#* To check the health of the API server, do nothing -> Returns Ok
+#*
+#* @get /healthCheck
+#* @serializer contentType list(type="text/plain; charset=UTF-8")
+function(res) {
+  future({
+    res$status <- 200
+    return("Ok")
+    })
+}
+
 #* @param in_xml:file Character string. Path and name of the file.
 #* @param IPC Name to search for Interprocess control (IPC) samples
 #* @param NC Name to search for Negative control (NC) samples
@@ -27,8 +43,12 @@ function(req) {
 #* @param barcodeB:file optional BarcodeB file
 #* @serializer text
 #* @post /normXML
-normXML <- function(in_xml, IPC=c("InterProcessControl"), NC=c("NegativeControl"), IC=c("mCherry"), barcodeB=""){
-  future_promise({
+normXML <- function(in_xml,
+                    IPC = c("InterProcessControl"),
+                    NC = c("NegativeControl"),
+                    IC = c("mCherry"),
+                    barcodeB = "") {
+  promises::future_promise({
     return(processXML(toString(in_xml), IPC, NC, IC, toString(barcodeB)))
   })
 }
@@ -50,7 +70,7 @@ xml2html <- function(res,
                      assayName = "NULISAseq 200-plex Inflammation Panel") {
   promises::future_promise({
     UUID <- uuid::UUIDgenerate()
-    tempFile <- paste0(UUID, '.Rmd')
+    tempFile <- paste0(UUID, ".Rmd")
     outFile <- paste0(UUID, ".html")
     file.copy(rmd_path, tempFile)
 
@@ -117,15 +137,4 @@ xml2pdf <- function(res,
     unlink(outFile)
     readBin(outFilePDF, "raw", n = file.info(outFilePDF)$size)
   })
-}
-
-#* To check the health of the API server, do nothing -> Returns Ok
-#*
-#* @get /healthCheck
-#* @serializer contentType list(type="text/plain; charset=UTF-8")
-function(res){
-  future({
-    res$status <- 200    
-    return("Ok")
-    })
 }
