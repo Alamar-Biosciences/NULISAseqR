372c093b6a37ce27184f20207aa3fb9c19726e91
dwightkuo <22648143+dwightkuo@users.noreply.github.com>
2023-04-27 16:05:13 -0700

    add simple test for rendering skeleton.Rmd and for validating the output of processXML with the schema

---
+++ tests/testthat/test-skeleton.R
@@ -0,0 +1,10 @@
+# Define a test for QCSampleCriteria
+test_that("skeleton.Rmd can be rendered into HTML", {
+  test_path <- paste0(testthat::test_path(), "./../inst/rmarkdown/templates/nulisaseq/skeleton/")
+  input <- paste0(test_path, "skeleton.Rmd")
+  output <- paste0(test_path, "skeleton.html")
+  withr::with_tempfile(output, {
+    rmarkdown::render(input)  
+    expect_true(file.exists(output), output) 
+  })
+})
