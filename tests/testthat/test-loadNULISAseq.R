9274023f3937f32387ed58c034d3444b2c13e115
dwightkuo <22648143+dwightkuo@users.noreply.github.com>
2023-08-08 23:24:43 -0700

    fix for SC issue: https://alamar-biosciences.monday.com/boards/3010094405/pulses/4955537388, also, change skeleton.Rmd so that it defaults to using the "type" column from BarcodeB to define {IPC, SC, NC}. Will need to make similar change to SAM'

--- tests/testthat/test-loadNULISAseq.R
+++ tests/testthat/test-loadNULISAseq.R
@@ -4,7 +4,7 @@ test_that("Test that loadNULISAseq reads in an XML file and adds lists of approp
   test_path <- paste0(testthat::test_path(), "./../inst/rmarkdown/templates/nulisaseq/skeleton/")
 
   input1 <- paste0(test_path, "20230325_TAP_Qatar_plate02_no_nonmatching_XML.xml")
-  data <- loadNULISAseq(input1, IPC='IPC', IC='mCherry')
+  data <- loadNULISAseq(input1, IPC=NULL, IC='mCherry', SC=NULL)
   expect_true(nrow(data$qcPlate) >= 5)
   expect_true(nrow(data$qcSample) > ncol(data$Data))
   expect_true(all(dim(data$IC_normed$normData) == dim(data$normed$interNormData)))
